316b3d0b6c730dbf9ac3ad6b93a9723d499fb67f
tinycaterpillar <jinik990305@gmail.com>
2022-08-22 02:17:27 +0900

    remove certain file

--- learn_python/preprocess/crawling_info.py
+++
@@ -1,9 +0,0 @@
-INFO = {
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36"
-}
-
-def load_problem_url():
-    with open("../source/problemURL.ktx", 'r+', encoding='utf-8') as f:
-        ret = set(map(lambda x: x.strip(), f.readlines()))
-
-    return ret
